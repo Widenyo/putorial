604ab5d3b0855861e6ab147493d0db87cb258f1d
Widenyo <misterhomunculo@gmail.com>
2022-08-18 05:51:07 -0300

    asfadfs

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const requestIp = require('request-ip')
 
 app.get('/', (req, res) => {
+
+  const ipe = requestIp.getClientIp(req)
+  console.log(ipe)
+
   res.send('PUTORIAL XD')
 })
 
 app.get('/aaa', (req, res) => {
+  const ipe = requestIp.getClientIp(req)
+  console.log(ipe)
+
   res.send('PUTORIAL /aaatest')
 })
 
